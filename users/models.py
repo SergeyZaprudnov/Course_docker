865ea9578adae8405f7f6193047b24d7809b7e88
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-05 21:29:52 +0300

    adding model description/users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from habbits.models import NULLABLE, NOT_NULLABLE
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Эмайл пользователя')
+    chat_id = models.IntegerField(unique=True, **NULLABLE, default=None)
+    telegram_user_name = models.CharField(max_length=150, **NOT_NULLABLE, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
