3209410cca8386afeda6a5bd725d5f68aafffd99
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-05 23:15:57 +0300

    editing

--- habbits/urls.py
+++ habbits/urls.py
@@ -6,10 +6,10 @@ app_name = "habbits"
 
 
 urlpatterns = [
-    path("habits/", HabbitListView.as_view(), name="show_all_habits"),
-    path("habit/<int:pk>/", HabbitDetailView.as_view(), name="habit_show"),
-    path("habit/create/", HabbitCreateView.as_view(), name="habit_create"),
-    path("habit/update/<int:pk>/", HabbitUpdateView.as_view(), name="habit_update"),
-    path("habit/delete/<int:pk>/", HabbitDeleteView.as_view(), name="habit_delete"),
-    path("share_habits/", ShareHabbitListView.as_view(), name="share_habits"),
+    path("habbits/", HabbitListView.as_view(), name="show_all_habbits"),
+    path("habbit/<int:pk>/", HabbitDetailView.as_view(), name="habbit_show"),
+    path("habbit/create/", HabbitCreateView.as_view(), name="habbit_create"),
+    path("habbit/update/<int:pk>/", HabbitUpdateView.as_view(), name="habbit_update"),
+    path("habbit/delete/<int:pk>/", HabbitDeleteView.as_view(), name="habbit_delete"),
+    path("share_habbits/", ShareHabbitListView.as_view(), name="share_habbits"),
 ]
